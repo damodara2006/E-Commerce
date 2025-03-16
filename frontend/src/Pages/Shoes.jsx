d3b9aadb9eaa809ce3c57505579588896608587d
damodara2006 <damodara2006@gmail.com>
2025-03-16 12:51:47 +0530

    Increase icon size in multiple components for better visibility

--- frontend/src/Pages/Shoes.jsx
+++ frontend/src/Pages/Shoes.jsx
@@ -26,7 +26,7 @@ function Men() {
   return (
     <div>
       <FaShoppingCart
-        className=" right-10 absolute top-7 hover:text-gray-400 transition-all"
+        className=" right-10 absolute top-7 hover:text-gray-400 text-3xl transition-all"
         onClick={() => navigate("/cart")}
       />
       <div>
