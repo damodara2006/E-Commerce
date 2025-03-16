22806356b5844928335c62cf5be4a75502e660ec
Damodara Prakash <165252354+damodara2006@users.noreply.github.com>
2025-03-16 09:25:45 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(cors(
     {
-        origin:"http://localhost:5173",
+        origin:"https://e-commerce-jjp0.onrender.com",
         credentials:true
     }
 ))
